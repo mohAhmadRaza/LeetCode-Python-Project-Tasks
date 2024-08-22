8139b3875d7346280094dcccc9017927d2a18f7a
AhmadRaza <158742870+mohAhmadRaza@users.noreply.github.com>
2024-08-22 10:34:36 +0500

    Create LeetCode-373-Find_k_Pairs_From_Array.py

---
+++ LeetCode-373-Find_k_Pairs_From_Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if not nums1 or not nums2 or k == 0:
+            return []
+
+        heap = []
+
+        for i in range(min(len(nums1), k)):
+            heapq.heappush(heap, (nums1[i] + nums2[0],i, 0))
+        
+        result = []
+        while heap and len(result) < k:
+            sum, i, j = heapq.heappop(heap)
+            result.append(([nums1[i], nums2[j]]))
+
+            if j + 1 < len(nums2):
+                heapq.heappush(heap, (nums1[i] + nums2[j+1],i, j+1))
+        
+        return result
