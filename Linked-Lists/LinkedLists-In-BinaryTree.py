38f0b30dab1ad950813a4d355db8e9ff21cca404
AhmadRaza <158742870+mohAhmadRaza@users.noreply.github.com>
2024-09-13 11:32:47 +0500

    Create LinkedLists-In-BinaryTree.py

---
+++ Linked-Lists/LinkedLists-In-BinaryTree.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        L = []
+        temp = head
+        while temp:
+            L.append(temp.val)
+            temp = temp.next
+        
+        def dfs(nod, idx):
+            if not nod:
+                return False
+            
+            if nod.val == L[idx]:
+                idx += 1
+                if idx == len(L):
+                    return True
+            if dfs(nod.left, idx) or dfs(nod.right, idx):
+                return True
+            return False
+
+        def check(node):
+            if not node:
+                return False
+            
+            if dfs(node, 0):
+                return True
+            
+            return check(node.left) or check(node.right)
+        
+        return check(root)
