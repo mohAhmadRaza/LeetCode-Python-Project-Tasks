fcc335fef944d559016a4c416e67ff25b708a309
AhmadRaza <158742870+mohAhmadRaza@users.noreply.github.com>
2025-07-16 16:00:21 +0500

    Create ProductExceptItself.py

---
+++ Suffix-PrefixPattern/ProductExceptItself.py
@@ -0,0 +1,15 @@
+def productExceptSelf(nums):
+    n = len(nums)
+    ans = [1] * n
+
+    prefix = 1
+    for i in range(n):
+        ans[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n-1, -1, -1):
+        ans[i] *= suffix
+        suffix *= nums[i]
+
+    return ans
