2b204a9c01619209d752f10bd7005e05943e8311
AhmadRaza <158742870+mohAhmadRaza@users.noreply.github.com>
2025-12-01 10:35:38 +0500

    Update movezeros.py

--- Two Pointers Algorithm/movezeros.py
+++ Two Pointers Algorithm/movezeros.py
@@ -8,3 +8,26 @@ class Solution:
                 left += 1
         
         return nums
+
+
+#--------------------------------------------------2ND SOLUTION----------------------------------------
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        if not nums:
+            return
+
+        left, right = 0, 1
+
+        while left < right and right < len(nums):
+            if nums[left] != 0:
+                left = left + 1
+                right = right + 1
+
+            elif nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left = left + 1
+                right = right + 1
+            else:
+                right = right + 1
+        
+
